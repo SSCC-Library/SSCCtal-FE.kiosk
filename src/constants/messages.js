9a8b8e03057f501365ed690b71a938e22bf1c6ab
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2025-04-14 23:00:01 +0900

    feat: 알림 메세지 상수로 선언

---
+++ src/constants/messages.js
@@ -0,0 +1,14 @@
+export const ERROR_MESSAGES = {
+	invalid_login: '학번 혹은 비밀번호를 잘못 입력하였습니다.',
+	login_fail: '로그인 요청 실패',
+};
+
+export const SUCCESS_MESSAGES = {
+	login_success: '숭실대학교 학생입니다.',
+	logout_success: '정상적으로 로그아웃 되었습니다.',
+};
+
+export const INFO_MESSAGES = {
+	redirecting: '10초 후 자동으로 이동합니다.',
+	too_many_attempts: '로그인 시도 횟수를 초과했습니다. {time}초 후 다시 시도하세요.',
+};
