6ee884428b4b44774f993308c2ee585ab5a89d97
Jeong <andy037200@gmail.com>
2025-04-16 21:51:18 +0900

    refactor: 라우팅 경로 추가 (#3)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/login/login-page';
 import MainPage from '@/pages/main/main-page';
+import QRPage from '@/pages/qrscan/qr-page';
 
 function Main() {
 	return (
 		<Routes>
 			<Route path="/" element={<LoginPage />} />
 			<Route path="/main" element={<MainPage />} />
+			<Route path="/qrscan" element={<QRPage />} />
 		</Routes>
 	);
 }
