ee010ecd9f9d8553c09606d72d88eca5d8b72e3c
Jeong <andy037200@gmail.com>
2025-04-18 15:10:19 +0900

    feat: 서버 연동 api 추가 (#5)

---
+++ src/api/user-api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const verifyUserBySchoolNumber = async (schoolnumber) => {
+	const res = await axios.post(`${BACKEND_URL}/user/by-schoolnumber`, {
+		schoolnumber,
+	});
+	return res.data;
+};
