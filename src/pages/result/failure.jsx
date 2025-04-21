77732543c19f156ccd4eb57a6c2bc1a6130b1375
Jeong <andy037200@gmail.com>
2025-04-21 20:36:48 +0900

    feat: 실패 페이지 구성 (#15)

---
+++ src/pages/result/failure.jsx
@@ -0,0 +1,34 @@
+import Button from '@/components/button';
+import './failure.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+import failureImg from '@/assets/failure.png';
+import Message from '@/components/message';
+import { useState, useEffect } from 'react';
+import { ERROR_MESSAGES } from '@/constants/messages';
+
+function FailurePage() {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [error, set_error] = useState('');
+	//const { mode, type, state } = location.state || {};
+	const mode = '대여';
+	const type = '도서';
+	const state = 'is_rental';
+
+	useEffect(() => {
+		set_error(ERROR_MESSAGES[state].replace('{type}', type));
+	}, []);
+
+	return (
+		<div className="failure-container">
+			<img src={failureImg} alt="실패 이미지" className="failure-image" />
+			<div className="title">{mode} 실패</div>
+			<Message type="message" text={error} class_name="result" />
+			<Button onClick={() => navigate('/main')} class_name="main-button">
+				확인
+			</Button>
+		</div>
+	);
+}
+
+export default FailurePage;
