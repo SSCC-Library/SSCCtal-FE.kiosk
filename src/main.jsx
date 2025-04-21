35c6a24d0392a371573c68bc6633d0507e868198
Jeong <andy037200@gmail.com>
2025-04-21 17:59:49 +0900

    refactor: success경로 추가 (#15)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/login/login_page';
 import MainPage from '@/pages/main/main_page';
 import QRPage from '@/pages/qrscan/qr_page';
+import SuccessPage from './pages/result/success';
 
 function Main() {
 	return (
@@ -9,6 +10,7 @@ function Main() {
 			<Route path="/" element={<LoginPage />} />
 			<Route path="/main" element={<MainPage />} />
 			<Route path="/qrscan" element={<QRPage />} />
+			<Route path="/result/success" element={<SuccessPage />} />
 		</Routes>
 	);
 }
