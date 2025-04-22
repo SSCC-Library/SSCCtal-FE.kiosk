94ad037cb9966c147772243347cbba96e413f6cb
Jeong <andy037200@gmail.com>
2025-04-22 17:32:44 +0900

    refactor: 로그인 인증 절차 변경 (#17)

--- src/api/user_api.js
+++ src/api/user_api.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const verifyUserBySchoolNumber = async (school_number) => {
-	const res = await axios.post(`${BACKEND_URL}/user/by-school-number`, {
-		school_number,
-	});
-	return res.data;
+export const login = async (school_number, password) => {
+	try {
+		const res = await axios.post(`${BACKEND_URL}/login`, {
+			school_number,
+			password,
+		});
+		return res.data;
+	} catch (err) {
+		if (err.response && err.response.data) {
+			return err.response.data;
+		} else {
+			return { status: 'fail', message: '서버와 통신 중 오류 발생' };
+		}
+	}
 };
