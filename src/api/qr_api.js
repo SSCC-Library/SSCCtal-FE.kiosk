7810c2b17bfdb37bff9d0e0d200461f5562bcfde
Jeong <andy037200@gmail.com>
2025-04-24 16:06:53 +0900

    refactor: api 주소 변경

--- src/api/qr_api.js
+++ src/api/qr_api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const qrScanStart = async () => {
-	const res = await axios.post(`${BACKEND_URL}/stream/start`);
+	const res = await axios.post(`${BACKEND_URL}/api/v0/stream/start`);
 	return res.data;
 };
