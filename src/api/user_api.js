6b31f4d2cdcea5f0e2522ae268cfff426af19bde
Jeong <andy037200@gmail.com>
2025-04-24 15:48:04 +0900

    refactor: post body, 주소 변경 (#17)

--- src/api/user_api.js
+++ src/api/user_api.js
@@ -4,15 +4,11 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const login = async (school_number, password) => {
 	try {
 		const res = await axios.post(`${BACKEND_URL}/login`, {
-			school_number,
-			password,
+			student_id: school_number,
+			password: password,
 		});
 		return res.data;
 	} catch (err) {
-		if (err.response && err.response.data) {
-			return err.response.data;
-		} else {
-			return { status: 'fail', message: '서버와 통신 중 오류 발생' };
-		}
+		throw { success: false, code: 500, data: null, message: '서버와 통신 중 오류 발생' };
 	}
 };
