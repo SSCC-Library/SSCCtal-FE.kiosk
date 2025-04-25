d8c23907e5ec32c04f5656805148514af66cab60
Jeong <andy037200@gmail.com>
2025-04-25 17:17:35 +0900

    refactor: mode에 따라 메세지 변경 (#19)

--- src/pages/result/success.jsx
+++ src/pages/result/success.jsx
@@ -6,15 +6,15 @@ import successImg from '@/assets/success.png';
 function SuccessPage() {
 	const navigate = useNavigate();
 	const location = useLocation();
-	//const { mode, type } = location.state || {};
-	const mode = '대여';
-	const type = '도서';
+	const { mode, type } = location.state;
+
 	return (
 		<div className="success-container">
 			<img src={successImg} alt="성공 이미지" className="success-image" />
-			<div className="title">{mode} 성공</div>
+			<div className="title">{mode === 'rental' ? '대여' : '반납'} 성공</div>
 			<div className="success-label">
-				{type} {mode === '대여' ? '대여가' : '반납이'} 완료되었습니다.
+				{type === 'book' ? '도서' : '물품'} {mode === 'rental' ? '대여가' : '반납이'}{' '}
+				완료되었습니다.
 			</div>
 			<Button onClick={() => navigate('/main')} class_name="main-button">
 				홈으로
