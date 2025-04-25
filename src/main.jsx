4e12a6b1898c31bb731bb2ef85eb027c18936ef9
Jeong <andy037200@gmail.com>
2025-04-25 16:41:57 +0900

    refactor: info 페이지 경로 추가 (#19)

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import MainPage from '@/pages/main/main_page';
 import QRPage from '@/pages/qrscan/qr_page';
 import SuccessPage from './pages/result/success';
 import FailurePage from './pages/result/failure';
+import InfoPage from './pages/info/info_page';
 
 function Main() {
 	return (
@@ -13,6 +14,7 @@ function Main() {
 			<Route path="/qrscan" element={<QRPage />} />
 			<Route path="/result/success" element={<SuccessPage />} />
 			<Route path="/result/failure" element={<FailurePage />} />
+			<Route path="/info" element={<InfoPage />} />
 		</Routes>
 	);
 }
