6c8ee00437d36e10a82d8a5c56c8a570ffa49d9b
Jeong <andy037200@gmail.com>
2025-04-26 19:08:13 +0900

    style: 변수 이름 변경 (#21)

--- src/hooks/use_get_date.js
+++ src/hooks/use_get_date.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 
 export function use_get_date() {
-	const [rental_string, set_rental_string] = useState('');
+	const [current_string, set_current_string] = useState('');
 	const [return_string, set_return_string] = useState('');
 
 	useEffect(() => {
 		const today = new Date();
 
-		// rental_string: yyyy-mm-dd/hh:mm:ss
-		const rental =
+		// current_string: yyyy-mm-dd/hh:mm:ss
+		const current =
 			today.getFullYear() +
 			'-' +
 			('0' + (today.getMonth() + 1)).slice(-2) +
@@ -20,7 +20,7 @@ export function use_get_date() {
 			('0' + today.getMinutes()).slice(-2) +
 			':' +
 			('0' + today.getSeconds()).slice(-2);
-		set_rental_string(rental);
+		set_current_string(current);
 
 		// return_string: 2주 후 yyyy-mm-dd
 		const return_date = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
@@ -33,5 +33,5 @@ export function use_get_date() {
 		set_return_string(return_str);
 	}, []);
 
-	return { rental_string, return_string };
+	return { current_string, return_string };
 }
