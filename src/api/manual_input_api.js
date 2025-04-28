c3d634504eebb667ecdb0da9a67143a25ea62dc8
Jeong <andy037200@gmail.com>
2025-04-28 13:54:23 +0900

    refactor: post 형식 변경

--- src/api/manual_input_api.js
+++ src/api/manual_input_api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const manual_input_id = async (item_id) => {
+export const manual_input_id = async (manual_id) => {
 	const res = await axios.post(`${BACKEND_URL}/api/v0/manual/input`, {
-		item_id: item_id,
+		isbn: manual_id,
 	});
 	return res.data;
 };
