63ed324c6d19637ab458e1368092f3127824c088
Jeong <andy037200@gmail.com>
2025-05-01 02:30:10 +0900

    design: qr스캔 페이지 디자인 변경 (#29)

--- src/pages/qrscan/qr_page.jsx
+++ src/pages/qrscan/qr_page.jsx
@@ -1,15 +1,12 @@
 import QRForm from './qr_form';
 import './qrscan.css';
-
+import { PageContainer } from '@/components/page_container';
 function QRPage() {
 	return (
-		<div className="qrscan-page">
-			<div className="qrscan-container">
-				<div className="qrscan-title">QR 코드 인식</div>
-				<div className="qrscan-label">카메라에서 10~15cm 떨어져 인식시켜주세요</div>
-				<QRForm />
-			</div>
-		</div>
+		<PageContainer title="QR 코드 인식" title_color="#003c9e">
+			<div className="qrscan-label">카메라에서 10~15cm 떨어져 인식시켜주세요</div>
+			<QRForm />
+		</PageContainer>
 	);
 }
 
