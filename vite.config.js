3a2fd41394247dd2797304d4fcb55c4dd29a2454
Jeong <andy037200@gmail.com>
2025-04-30 20:13:34 +0900

    chore: 빌드 위해 config 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
-
-	return {
-		plugins: [react()],
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, './src'),
-			},
+export default defineConfig({
+	base: './',
+	build: {
+		target: 'es2015',
+		outDir: 'dist',
+		assetsDir: 'assets',
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
 		},
-		// server: {
-		// 	proxy: {
-		// 		'/api/ssu': {
-		// 			target: env.VITE_SSU_PROXY_TARGET,
-		// 			changeOrigin: true,
-		// 			secure: false,
-		// 			rewrite: (path) => path.replace(/^\/api\/ssu/, '/Symtra_sso'),
-		// 		},
-		// 	},
-		// },
-	};
+	},
 });
