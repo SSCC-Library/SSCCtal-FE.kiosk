8d2d17ee02ac330ac9d3631ff85009d05676e8ce
Jeong <andy037200@gmail.com>
2025-05-01 02:07:36 +0900

    design: 로그인 페이지 디자인 수정 (#29)

--- src/pages/login/login_page.jsx
+++ src/pages/login/login_page.jsx
@@ -1,21 +1,18 @@
 import LoginForm from './login_form';
 import './login.css';
 import { useEffect } from 'react';
-
+import { PageContainer } from '../../components/page_container';
 function LoginPage() {
 	useEffect(() => {
 		localStorage.removeItem('user');
-		console.log('로그아웃 처리');
 		localStorage.removeItem('item');
-		console.log('아이템 삭제');
 	}, []);
 
 	return (
-		<div className="login-container">
-			<div className="login-title">SSCCtal</div>
+		<PageContainer title="SSCCtal" title_color="#2e5bff">
 			<div className="login-label">유세인트 계정을 입력하세요</div>
 			<LoginForm />
-		</div>
+		</PageContainer>
 	);
 }
 
