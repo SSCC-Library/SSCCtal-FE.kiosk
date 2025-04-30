f10eb6762a2c1a45eff721abc97d8f4651e52e25
Jeong <andy037200@gmail.com>
2025-04-30 15:41:02 +0900

    refactor: jsx용 포멧 변경 (#27)

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,9 +1,13 @@
 import './message.css';
 
-function Message({ text, type = 'info', class_name = '' }) {
+function Message({ type = 'info', text, class_name = '' }) {
 	if (!text) return null;
 
-	return <div className={`message ${type} ${class_name}`}>{text}</div>;
+	return (
+		<div className={`message ${type} ${class_name}`}>
+			{typeof text === 'string' ? <p>{text}</p> : text}
+		</div>
+	);
 }
 
 export default Message;
