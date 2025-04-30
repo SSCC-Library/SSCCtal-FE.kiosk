d48716681adb25d3826e6f462793d0d20d3af580
Jeong <andy037200@gmail.com>
2025-05-01 02:08:00 +0900

    design: 메인 페이지 디자인 수정 (#29)

--- src/pages/main/main_form.jsx
+++ src/pages/main/main_form.jsx
@@ -4,11 +4,26 @@ import './main.css';
 import { useNavigate } from 'react-router-dom';
 import { INFO_MESSAGES } from '@/constants/messages';
 import AlertModal from '@/components/alert_modal';
-
+import { qrScanStart } from '../../api/qr_api';
 function MainForm() {
 	const navigate = useNavigate();
 	const [is_open, set_is_open] = useState(false);
 	const [message, set_message] = useState('');
+
+	const handle_qr_scan = async () => {
+		try {
+			const res = await qrScanStart();
+			if (res.success === false) {
+				alert('QR 인식이 실패하였습니다. ISBN을 직접 입력해주세요');
+				set_is_id_input(true);
+			} else if (res.success === true) {
+				handle_item_flow(res);
+			}
+		} catch (err) {
+			console.error('QR 요청 실패:', err);
+			alert('요청 중 오류 발생');
+		}
+	};
 
 	return (
 		<div className="main-button-wrapper">
@@ -25,6 +40,12 @@ function MainForm() {
 				>
 					반납
 				</Button>
+				{/* <Button onClick={() => handle_qr_scan()} class_name="square-button">
+					대여
+				</Button>
+				<Button onClick={() => handle_qr_scan()} class_name="square-button">
+					반납
+				</Button> */}
 			</div>
 			<Button onClick={() => navigate('/')} class_name="default-button">
 				로그아웃
--- src/pages/main/main_page.jsx
+++ src/pages/main/main_page.jsx
@@ -1,17 +1,17 @@
 import MainForm from './main_form';
 import './main.css';
 import { use_user } from '@/hooks/use_user';
+import { PageContainer } from '../../components/page_container';
 function MainPage() {
 	const user = use_user();
 
 	if (!user) return null;
 
 	return (
-		<div className="main-container">
-			<div className="main-title">SSCCtal</div>
+		<PageContainer title="SSCCtal" title_color="#2e5bff">
 			<div className="main-label">{user.name}님 환영합니다</div>
 			<MainForm />
-		</div>
+		</PageContainer>
 	);
 }
 /*function MainPage() {
