e9cc288c768c17253686a69ccca1b2400d2e51b6
Jeong <andy037200@gmail.com>
2025-05-04 01:34:40 +0900

    refactor: 문의하기 버튼 코드 page로 변경 (#29)

--- src/pages/login/login_page.jsx
+++ src/pages/login/login_page.jsx
@@ -1,17 +1,42 @@
 import LoginForm from './login_form';
 import './login.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { PageContainer } from '@/components/page_container';
+import Button from '@/components/button';
+import { format_message } from '@/utils/format_message';
+import { INFO_MESSAGES } from '@/constants/messages';
+import AlertModal from '@/components/alert_modal';
+
 function LoginPage() {
 	useEffect(() => {
 		localStorage.removeItem('user');
 		localStorage.removeItem('item');
 	}, []);
+	const [message, set_message] = useState('');
+	const [is_open, set_is_open] = useState(false);
 
 	return (
 		<PageContainer title="SSCCtal" title_color="#2e5bff">
 			<div className="login-label">유세인트 계정을 입력하세요</div>
 			<LoginForm />
+			<Button
+				onClick={() => {
+					set_message(format_message(INFO_MESSAGES.alert_admin));
+					set_is_open(true);
+				}}
+				class_name="admin-button"
+			>
+				문의하기
+			</Button>
+			{is_open && (
+				<AlertModal
+					message={message}
+					on_close={() => {
+						set_message('');
+						set_is_open(false);
+					}}
+				/>
+			)}
 		</PageContainer>
 	);
 }
