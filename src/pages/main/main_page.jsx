05eab46672b0170bab50ae71098c58587a6036b6
Jeong <andy037200@gmail.com>
2025-06-27 19:17:25 +0900

    refactor: 메인 페이지 리팩터링 (#31) - 코드 설명 주석 추가 - qrscan 관련 코드 삭제

--- src/pages/main/main_page.jsx
+++ src/pages/main/main_page.jsx
@@ -1,12 +1,17 @@
+/*
+메인 페이지
+- 비로그인 접근 시 로그인 페이지로 이동(use_user)
+*/
+
+import { useState } from 'react';
+import AlertModal from '@/components/alert_modal';
+import Button from '@/components/Button';
+import { PageContainer } from '@/components/page_container';
+import { INFO_MESSAGES } from '@/constants/messages';
+import { use_user } from '@/hooks/use_user';
+import { format_message } from '@/utils/format_message';
 import MainForm from './main_form';
 import './main.css';
-import { use_user } from '@/hooks/use_user';
-import { PageContainer } from '@/components/page_container';
-import Button from '@/components/Button';
-import AlertModal from '@/components/alert_modal';
-import { INFO_MESSAGES } from '@/constants/messages';
-import { useState } from 'react';
-import { format_message } from '@/utils/format_message';
 
 function MainPage() {
 	const user = use_user();
@@ -32,21 +37,23 @@ function MainPage() {
 		</PageContainer>
 	);
 }
-/*function MainPage() {
-	const location = useLocation();
-	const user = location.state?.user;
 
-	return (
-		<div className="main-page">
-			<div className="main-container">
-				<div className="title">SCLibrary</div>
-				<div className="label">
-					{user ? `${user.name}님 환영합니다` : '비로그인 상태 (개발 중)'}
-				</div>
-				<MainForm />
-			</div>
-		</div>
-	);
-}*/
+// //비로그인 시 코드
+// function MainPage() {
+// 	const location = useLocation();
+// 	const user = location.state?.user;
+
+// 	return (
+// 		<div className="main-page">
+// 			<div className="main-container">
+// 				<div className="title">SCLibrary</div>
+// 				<div className="label">
+// 					{user ? `${user.name}님 환영합니다` : '비로그인 상태 (개발 중)'}
+// 				</div>
+// 				<MainForm />
+// 			</div>
+// 		</div>
+// 	);
+// }
 
 export default MainPage;
