0fd8569bd8412ec3aee20783fb11339d77159c5e
Jeong <andy037200@gmail.com>
2025-06-30 13:20:50 +0900

    comment: 코드 설명 주석 추가 (#31)

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,8 @@
+/*
+ReactDOM에서 루트에 렌더링(앱 진입점)
+전체 라우팅을 HashRouter(webOS 환경) + Main 컴포넌트로 분리
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
