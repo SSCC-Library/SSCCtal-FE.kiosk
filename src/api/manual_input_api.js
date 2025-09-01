5156f3590a1e9d9b826b9ddc81c40517e34ff1cf
jyi0226 <andy0372@naver.com>
2025-09-01 11:47:06 +0900

    refactor: api 인증 헤더 추가

--- src/api/manual_input_api.js
+++ src/api/manual_input_api.js
@@ -5,10 +5,14 @@ QR 인식 오류 시 직접 입력한 ISBN 보내는 API
 import axios from 'axios';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const ADMIN_TOKEN = localStorage.getItem('token');
 
+const AUTH_HEADER = {
+	headers: {
+		Authorization: `Bearer ${ADMIN_TOKEN}`,
+	},
+};
 export const manual_input_id = async (isbn) => {
-	const res = await axios.post(`${BACKEND_URL}/api/v0/manual/input`, {
-		isbn: isbn,
-	});
+	const res = await axios.get(`${BACKEND_URL}/api/v1/kiosk/input/${isbn}`, AUTH_HEADER);
 	return res.data;
 };
--- src/api/send_result_api.js
+++ src/api/send_result_api.js
@@ -6,21 +6,17 @@
 
 import axios from 'axios';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const ADMIN_TOKEN = localStorage.getItem('token');
 
-export const send_result = async (student_id, item_id, current_date, return_date, mode) => {
-	const url = mode === 'rental' ? '/api/v0/rental' : '/api/v0/return';
-	const data = {
-		studend_id: student_id,
-		item_id: item_id,
-	};
+const AUTH_HEADER = {
+	headers: {
+		Authorization: `Bearer ${ADMIN_TOKEN}`,
+	},
+};
 
-	if (mode === 'rental') {
-		data.rental_date = current_date;
-		data.expectation_return_date = return_date;
-	} else {
-		data.return_date = current_date;
-	}
+export const send_result = async (identifier_code, mode) => {
+	const url = mode === 'rental' ? '/api/v1/kiosk/rent' : '/api/v1/kiosk/return';
 
-	const res = await axios.post(`${BACKEND_URL}${url}`, data);
+	const res = await axios.post(`${BACKEND_URL}${url}/${identifier_code}`, AUTH_HEADER);
 	return res.data;
 };
